d57869b6a8c74fb319aab1a1821bf486b05bb62e
Tony Marschall <tnix@dev.paupau.de>
2011-11-04 12:11:39 +0100

    fixed sql limit and close dates on reopen tickets

--- lib/redmine_closed_column/patches/issue_patch.rb
+++ lib/redmine_closed_column/patches/issue_patch.rb
@@ -3,23 +3,19 @@ module RedmineClosedColumn
     module IssuePatch
       
       def first_closed_date
-	if IssueStatus.find(status_id).is_closed
-          _sql = "SELECT journals.created_on FROM journals, journal_details WHERE journals.journalized_id = #{id} AND journals.id = journal_details.journal_id AND journal_details.prop_key = 'status_id' AND journal_details.property = 'attr' AND journal_details.value IN (SELECT is_close.id FROM issue_statuses is_close WHERE is_close.is_closed = 1) ORDER BY journals.created_on DESC LIMIT 1,1"
-	  result = ActiveRecord::Base.connection.select_value _sql
-          unless result.nil?
-            @first_closed_date ||= result
-          end
-	end
+        _sql = "SELECT journals.created_on FROM journals, journal_details WHERE journals.journalized_id = #{id} AND journals.id = journal_details.journal_id AND journal_details.prop_key = 'status_id' AND journal_details.property = 'attr' AND journal_details.value IN (SELECT is_close.id FROM issue_statuses is_close WHERE is_close.is_closed = 1) ORDER BY journals.created_on DESC LIMIT 1"
+	    result = ActiveRecord::Base.connection.select_value _sql
+        unless result.nil?
+          @first_closed_date ||= result
+        end
       end
 
       def last_closed_date
-	if IssueStatus.find(status_id).is_closed
-          _sql = "SELECT journals.created_on FROM journals, journal_details WHERE journals.journalized_id = #{id} AND journals.id = journal_details.journal_id AND journal_details.prop_key = 'status_id' AND journal_details.property = 'attr' AND journal_details.value IN (SELECT is_close.id FROM issue_statuses is_close WHERE is_close.is_closed = 1) ORDER BY journals.created_on LIMIT 1,1"
-	  result = ActiveRecord::Base.connection.select_value _sql
-          unless result.nil?
-            @last_closed_date ||= result
-          end
-	end
+        _sql = "SELECT journals.created_on FROM journals, journal_details WHERE journals.journalized_id = #{id} AND journals.id = journal_details.journal_id AND journal_details.prop_key = 'status_id' AND journal_details.property = 'attr' AND journal_details.value IN (SELECT is_close.id FROM issue_statuses is_close WHERE is_close.is_closed = 1) ORDER BY journals.created_on LIMIT 1"
+	    result = ActiveRecord::Base.connection.select_value _sql
+        unless result.nil?
+          @last_closed_date ||= result
+        end
       end
       
     end
